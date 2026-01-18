a62934f7ffe0e749ca5719097a59a6ec4438e0ff
joppe <jboeve009@gmail.com>
2026-01-18 15:13:32 +0100

    ft_write kinda works

--- lib/src/ft_write.s
+++ lib/src/ft_write.s
@@ -1,17 +1,9 @@
-SECTION .rodata
-msg: db "Test 123", 10
-msg_len: equ $-msg
-
 SECTION .text
 ; make `_test_func` a global symbol to expose it to the linker
 global ft_write
 
-
-; ssize_t ft_write(int fildes, const void *buf, size_t nbyte);
 ft_write:
 	mov rax, 1
-	mov rdi, 1
-	mov	rsi, $msg
-	mov rdx, $msg_len
 	syscall
-	ret ; rax or rdx
+	mov rax, rdx
+	ret
--- unit_tests/test_ft_write.c
+++ unit_tests/test_ft_write.c
@@ -4,11 +4,16 @@
 #include <criterion/internal/assert.h>
 #include <criterion/types.h>
 #include <stdint.h>
+#include <string.h>
+#include <unistd.h>
 
 #define OurTest(...) Test(ft_strlen, ...);
 
 Test(ft_strlen, strlen_slen)
 {
-    int64_t res = ft_write();
-    cr_assert(res == 2147483648, "Got result: %ld\n", res);
+	const char *s = "teststring123";
+	const ssize_t s_len = strlen(s);
+
+    ssize_t res = ft_write(1, s, s_len);
+    cr_assert(res == s_len, "Got result: %ld\n", res);
 }
