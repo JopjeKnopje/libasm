c021635087ba6c49b0bc180b3e0d948b4726f9ab
joppe <jboeve009@gmail.com>
2026-01-21 01:17:06 +0100

    Can write to pointer babyyyy

--- lib/libasm/src/ft_write.s
+++ lib/libasm/src/ft_write.s
@@ -1,3 +1,7 @@
+extern __errno_location
+
+
+
 SECTION .text
 ; make `_test_func` a global symbol to expose it to the linker
 global ft_write
@@ -5,5 +9,16 @@ global ft_write
 ft_write:
 	mov rax, 1
 	syscall
-	mov rax, rdx
+	; if `rax` is negative we put `-1` in `rax` and set errno to the positive value
+	test rax, rax
+
+    call __errno_location wrt ..plt
+
+	; the de-referenced data at address `rax` is `QWORD`
+    mov    qword [rax], 123
+
 	ret
+
+__handle_error:
+	mov rax, 4
+	syscall
