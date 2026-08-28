c1e19bede4855520fd821d84cc7dcaf49940005c
joppe <jopjeknopje@proton.me>
2026-08-29 00:58:45 +0200

    WIP: Having trouble derefferencing rax value using r10 offset

--- lib/libasm/src/ft_strlen.s
+++ lib/libasm/src/ft_strlen.s
@@ -5,10 +5,12 @@ ft_strlen:
 	; init our variable to 0
 	mov r10, 0
 loop:
+	; get char value from pointer passed into the function.
+	mov rbx, [rax + r10]
+	; increment our counter.
 	add r10, 1
-	mov bx, [rax + r10]
 	; ZF gets set when `BX` is 0
-	cmp bx, 0
+	cmp rbx, 0
 	; jump back to loop when "not zero" aka when ZF is not set.
 	jnz loop
 
