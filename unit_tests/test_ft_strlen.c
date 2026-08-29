5c8297f3ba0267d7d46935487b7ad6bb3d42d624
joppe <jopjeknopje@proton.me>
2026-08-29 23:45:31 +0200

    feat(ft_strlen): add crash test

--- unit_tests/test_ft_strlen.c
+++ unit_tests/test_ft_strlen.c
@@ -2,43 +2,48 @@
 #include "string.h"
 #include "unity.h"
 #include <errno.h>
+#include <signal.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 void test_valid_string(void)
 {
-    char *s = "test123";
+    const char *s = "123";
 
-    int expected = strlen(s);
-    int actual = ft_strlen(s);
+    const int expected = strlen(s);
+    const int actual = ft_strlen(s);
 
     TEST_ASSERT_EQUAL_INT32_MESSAGE(expected, actual, "output values don't match");
 }
 
 void test_null_should_crash(void)
 {
-    // char *s = NULL;
+    const char *s = NULL;
 
-    // TODO: Fork and except segv
+    const pid_t pid = fork();
+    TEST_ASSERT_NOT_EQUAL_INT32_MESSAGE(-1, pid, strerror(errno));
 
-	int pid = fork();
-	TEST_ASSERT_NOT_EQUAL_INT32_MESSAGE(-1, pid, strerror(errno));
+    // child proc
+    if (pid == 0)
+    {
+        // this should crash
+        int x = ft_strlen(s);
+        // we need this cast otherwise the compiler will optimize our function call away,
+        (void)x;
+        // exit as a fallback
+        exit(1);
+    }
+    int status;
+    const pid_t code = waitpid(pid, &status, 0);
+    TEST_ASSERT_NOT_EQUAL_INT32_MESSAGE(-1, code, "error waitpid returned %d [%s]\n", code,
+                                        strerror(errno));
 
-	// child
-	if (pid == 0)
-	{
+    TEST_ASSERT_TRUE_MESSAGE(WIFSIGNALED(status), "strlen process did not exit with signal");
 
-		// this should crash
-		// (void)strlen(s);
-		printf("%d\n", pid);
-	}
-	printf("%d\n", pid);
-	int status;
-	waitpid(pid, &status, 0);
-
-	// TEST_ASSERT_GREATER_OR_EQUAL_INT32_MESSAGE(1, WIFSIGNALED(status), "child did not exit with a signal");
-	// int exit_sig = WTERMSIG(status) + 128;
-	printf("exit sig %d\n", exit_sig);
-
+    const int exit_sig = WTERMSIG(status);
+    TEST_ASSERT_TRUE_MESSAGE(exit_sig == SIGSEGV, "strlen exited with signal [%s] expected [%s]\n",
+                             strsignal(exit_sig), strsignal(SIGSEGV));
 }
