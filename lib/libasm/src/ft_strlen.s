71b6c9ae6732d16e923619c73725346e07cbde0c
joppe <jopjeknopje@proton.me>
2026-08-29 19:55:08 +0200

    WIP: dsada

--- lib/libasm/src/ft_strlen.s
+++ lib/libasm/src/ft_strlen.s
@@ -3,22 +3,13 @@ global ft_strlen
 
 ft_strlen:
 	; init our variable to 0
-	cmp rdi, 0
-	jz end
 	xor r10, r10
 loop:
-	; get char value from pointer passed into the function.
-	; mov rbx, [rdi + r10]
-	; increment our counter.
-	inc r10
-	; ZF gets set when `BX` is 0
+	; ZF is set when `RDI + r10` is 0
 	cmp byte [rdi + r10], 0
+	inc r10
 	; jump back to loop when "not zero" aka when ZF is not set.
 	jnz loop
 
 	mov rax, r10
-	ret
-
-end:
-	mov rax, 0
 	ret
--- unit_tests/test_ft_strlen.c
+++ unit_tests/test_ft_strlen.c
@@ -1,6 +1,10 @@
 #include "libasm.h"
 #include "string.h"
 #include "unity.h"
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 void test_valid_string(void)
 {
@@ -12,12 +16,29 @@ void test_valid_string(void)
     TEST_ASSERT_EQUAL_INT32_MESSAGE(expected, actual, "output values don't match");
 }
 
-void test_null_string(void)
+void test_null_should_crash(void)
 {
-    char *s = NULL;
+    // char *s = NULL;
 
-    // int expected = strlen(s);
-    (void)ft_strlen(s);
+    // TODO: Fork and except segv
 
-    // TEST_ASSERT_EQUAL_INT32_MESSAGE(expected, actual, "output values don't match");
+	int pid = fork();
+	TEST_ASSERT_NOT_EQUAL_INT32_MESSAGE(-1, pid, strerror(errno));
+
+	// child
+	if (pid == 0)
+	{
+
+		// this should crash
+		// (void)strlen(s);
+		printf("%d\n", pid);
+	}
+	printf("%d\n", pid);
+	int status;
+	waitpid(pid, &status, 0);
+
+	// TEST_ASSERT_GREATER_OR_EQUAL_INT32_MESSAGE(1, WIFSIGNALED(status), "child did not exit with a signal");
+	// int exit_sig = WTERMSIG(status) + 128;
+	printf("exit sig %d\n", exit_sig);
+
 }
