a83f37b7fe96c6b08acbf13741d1573deface58d
joppe <jopjeknopje@proton.me>
2026-08-30 17:59:06 +0200

    WIPasd

--- lib/libasm/src/ft_strcmp.s
+++ lib/libasm/src/ft_strcmp.s
@@ -1,24 +1,20 @@
 SECTION .text
 global ft_strcmp
 
-; rdi dst
-; rsi src
 ft_strcmp:
 	xor rcx, rcx
 .loop:
-	; move s1 into a tmp register
-	mov r10, [rdi + rcx]
-	; sub s1 from s2 and store result in s1
-	sub r10, [rsi + rcx]
+	; move s1 into a tmp register.
+	mov r10b, byte [rdi + rcx]
+	; sub s1 from s2 and store result in s1.
+	sub r10b, byte [rsi + rcx]
+	; check if there was a diff between s1 and s2.
+	cmp r10b, 0
+	jnz .exit
 
-	cmp r10, 0
-	jnz .done
-
-	mov byte r10, [rsi + rcx]
-	mov qword [rdi + rcx], byte r10
 	inc rcx
 	jmp .loop
 
-.done:
-	mov rax, r10
+.exit:
+	mov al, r10b
 	ret
