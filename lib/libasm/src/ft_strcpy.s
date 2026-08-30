0ece2a893654dd2f9311ef6e9abd7b0b870246eb
Joppe Boeve <jboeve@f1r7s2.codam.nl>
2026-08-30 15:29:15 +0200

    one shotted strcpy?

---
+++ lib/libasm/src/ft_strcpy.s
@@ -0,0 +1,21 @@
+extern __errno_location
+
+SECTION .text
+global ft_strcpy
+
+; rdi dst
+; rsi src
+ft_strcpy:
+	xor rcx, rcx
+.loop:
+	; check if we're at the end of src
+	cmp byte [rsi + rcx], 0
+	je .done
+	mov r10, [rsi + rcx]
+	mov [rdi + rcx], r10
+	inc rcx
+	jmp .loop
+
+.done:
+	mov rax, rdi
+	ret
--- unit_tests/test_ft_strcpy.c
+++ unit_tests/test_ft_strcpy.c
@@ -1,9 +1,21 @@
 #include "libasm.h"
+#include <stddef.h>
+#include <string.h>
+#include <strings.h>
+#include "unity.h"
 
 
 
-void test() 
+void test_copy()
 {
 	size_t SIZE = 32;
-	char s1[SIZE];
+	const char *src = "123";
+	char dst[SIZE];
+	bzero(dst, SIZE * sizeof(char));
+
+	char *s = ft_strcpy(dst, src);
+
+	TEST_ASSERT_EQUAL_PTR_MESSAGE(&dst, s, "return value is not equal to dst pointer");
+	TEST_ASSERT_EQUAL_CHAR_ARRAY(src, dst, strlen(src));
+	printf("%s\n", dst);
 }
