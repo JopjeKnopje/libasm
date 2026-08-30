49d2bd8ae7c0d644be6b4b1ed9d2ea80f8128ae5
Joppe Boeve <jboeve@f1r7s2.codam.nl>
2026-08-30 16:51:34 +0200

    add overflow test

--- unit_tests/test_ft_strcpy.c
+++ unit_tests/test_ft_strcpy.c
@@ -1,21 +1,31 @@
 #include "libasm.h"
+#include "unity.h"
 #include <stddef.h>
 #include <string.h>
 #include <strings.h>
-#include "unity.h"
-
-
 
 void test_copy()
 {
-	size_t SIZE = 32;
-	const char *src = "123";
-	char dst[SIZE];
-	bzero(dst, SIZE * sizeof(char));
+    size_t SIZE = 32;
+    const char *src = "test123";
+    char dst[SIZE];
+    bzero(dst, SIZE * sizeof(char));
 
-	char *s = ft_strcpy(dst, src);
+    char *s = ft_strcpy(dst, src);
 
-	TEST_ASSERT_EQUAL_PTR_MESSAGE(&dst, s, "return value is not equal to dst pointer");
-	TEST_ASSERT_EQUAL_CHAR_ARRAY(src, dst, strlen(src));
-	printf("%s\n", dst);
+    TEST_ASSERT_EQUAL_PTR_MESSAGE(&dst, s, "return value is not equal to dst pointer");
+    TEST_ASSERT_EQUAL_CHAR_ARRAY(src, dst, strlen(src));
+    printf("%s\n", dst);
+}
+
+void test_copy_overflow()
+{
+    size_t SIZE = 1;
+    const char *src = "123";
+    char dst[SIZE];
+
+    char *s = ft_strcpy(dst, src);
+
+    TEST_ASSERT_EQUAL_PTR_MESSAGE(&dst, s, "return value is not equal to dst pointer");
+    TEST_ASSERT_EQUAL_CHAR_ARRAY_MESSAGE(src, dst, strlen(src), "overflow failed");
 }
