0cdbb18b53aa146392c42b2bb157dfe91272df9b
joppe <jopjeknopje@proton.me>
2026-09-17 23:23:29 +0200

    add more strdup tests

--- unit_tests/test_ft_strdup.c
+++ unit_tests/test_ft_strdup.c
@@ -1,41 +1,45 @@
+#include "big_str.h"
 #include "libasm.h"
 #include "unity.h"
-#include <asm-generic/errno-base.h>
-#include <errno.h>
-#include <malloc.h>
 #include <stdint.h>
-#include <stdio.h>
 #include <string.h>
 
-#define OK (0)
-
-// void test_malloc_fail(void)
-// {
-// 	// make sure errno is not set
-//     TEST_ASSERT_EQUAL_INT32_MESSAGE(OK, errno, "errno is already set to [%s]", strerror(errno));
-//
-//     void *ptr = ft_malloc_wrap(PTRDIFF_MAX);
-// 	(void) ptr;
-//     // 1. check if `errno` has been set by our function.
-//     TEST_ASSERT_EQUAL_INT32_MESSAGE(ENOMEM, errno, "expected errno [%s] got [%s]",
-//     strerror(ENOMEM), strerror(errno));
-//
-// }
-
-// TODO: Figure out how to monitor malloced size
-void test_malloc_info()
+void test_strdup_check_len()
 {
-    char *test = strdup("test123");
+    char *test = "test123";
     char *s = ft_strdup(test);
-    int64_t x = (int64_t)s;
 
-    printf("[%s]\n", s);
-    printf("%ld\n", x);
+    TEST_ASSERT_NOT_NULL(s);
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(strlen(s), strlen(test),
+                                    "output string length does not match input");
+}
 
-    // void *x = (int32_t *) s;
-    //
-    // printf("%u\n", (int32_t) x);
+void test_strdup_big_str()
+{
+    char *test = BIG_STR;
+    char *s = ft_strdup(test);
 
-    // s[2100] = 't';
-    (void)s;
+    TEST_ASSERT_NOT_NULL(s);
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(strlen(s), strlen(test),
+                                    "output string length does not match input");
+}
+
+void test_strdup_empty()
+{
+    char *test = "";
+    char *s = ft_strdup(test);
+
+    TEST_ASSERT_NOT_NULL(s);
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(strlen(s), strlen(test),
+                                    "output string length does not match input");
+}
+
+void test_strdup_terminated()
+{
+    char *test = "part\0ofastring";
+    char *s = ft_strdup(test);
+
+    TEST_ASSERT_NOT_NULL(s);
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(strlen(s), strlen(test),
+                                    "output string length does not match input");
 }
